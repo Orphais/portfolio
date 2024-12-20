ac6f6e7633a2ff28af5242a4ccf53f8b57fa287d
JeanBon-dev <hugues.porchet@orange.fr>
2024-12-20 14:20:37 +0100

    Suppression d'un commentaire

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -10,6 +10,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Csrf\CsrfToken;
 
 class CommentController extends AbstractController
 {
@@ -41,4 +43,38 @@ class CommentController extends AbstractController
 
         return $this->redirectToRoute('creation_show', ['id' => $creationId]);
     }
+
+    #[Route('/comment/delete/{id}', name: 'comment_delete', methods: ['POST'])]
+public function deleteComment(
+    int $id,
+    Request $request,
+    EntityManagerInterface $entityManager,
+    CsrfTokenManagerInterface $csrfTokenManager
+): Response {
+    $comment = $entityManager->getRepository(Comment::class)->find($id);
+
+    if (!$comment) {
+        throw $this->createNotFoundException('Commentaire non trouvé.');
+    }
+
+    $csrfToken = new CsrfToken('delete' . $comment->getId(), $request->request->get('_token'));
+    if (!$csrfTokenManager->isTokenValid($csrfToken)) {
+        throw $this->createAccessDeniedException('Token CSRF invalide.');
+    }
+
+    $user = $this->getUser();
+    if ($comment->getUser() !== $user) {
+        $this->addFlash('error', 'Vous ne pouvez supprimer que vos propres commentaires.');
+        return $this->redirect($request->headers->get('referer') ?? $this->generateUrl('creation.index'));
+    }
+
+    $entityManager->remove($comment);
+    $entityManager->flush();
+
+    $this->addFlash('success', 'Commentaire supprimé avec succès.');
+
+    // Redirection vers la page d'où provient la requête
+    $referer = $request->headers->get('referer');
+    return $this->redirect($referer ?? $this->generateUrl('creation.index'));
+}
 }
